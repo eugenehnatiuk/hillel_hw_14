8970e6f9dc9a8fa835178ccef44ffa32d82ae58a
Eugene Hnatiuk <gnatyuk.e@gmail.com>
2023-10-16 12:17:16 +0300

    changed logic of passeye on focus/blur

--- passeye.js
+++ passeye.js
@@ -4,17 +4,24 @@ passWrappers.forEach((block) => {
   const passEye = block.querySelector(".passeye");
   const passwordInput = block.querySelector("input[type='password']");
 
-  passEye.addEventListener("mouseenter", () => {
-    passEye.classList.add("visible");
-  });
+  // passEye.addEventListener("mouseenter", () => {
+  //   passEye.classList.add("visible");
+  // });
 
-  passEye.addEventListener("mouseleave", () => {
-    passEye.classList.remove("visible");
-    passwordInput.type = "password";
-  });
+  // passEye.addEventListener("mouseleave", () => {
+  //   passEye.classList.remove("visible");
+  // passwordInput.type = "password";
+  // });
 
   passEye.addEventListener("click", () => {
     passwordInput.type =
       passwordInput.type === "password" ? "text" : "password";
+    passEye.classList.toggle("visible");
+    passwordInput.focus();
+  });
+
+  passwordInput.addEventListener("blur", () => {
+    passwordInput.type = "password";
+    passEye.classList.remove("visible");
   });
 });
