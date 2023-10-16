12612bffdb13f30f395f5ade30781706f874e148
Eugene Hnatiuk <gnatyuk.e@gmail.com>
2023-10-16 14:08:50 +0300

    password input behaviorr correction and color change

--- passeye.js
+++ passeye.js
@@ -3,25 +3,20 @@ const passWrappers = document.querySelectorAll(".pass_wrapper");
 passWrappers.forEach((block) => {
   const passEye = block.querySelector(".passeye");
   const passwordInput = block.querySelector("input[type='password']");
-
-  // passEye.addEventListener("mouseenter", () => {
-  //   passEye.classList.add("visible");
-  // });
-
-  // passEye.addEventListener("mouseleave", () => {
-  //   passEye.classList.remove("visible");
-  // passwordInput.type = "password";
-  // });
-
-  passEye.addEventListener("click", () => {
-    passwordInput.type =
-      passwordInput.type === "password" ? "text" : "password";
-    passEye.classList.toggle("visible");
-    passwordInput.focus();
-  });
+  let passwordVisible = false;
 
   passwordInput.addEventListener("blur", () => {
+    passwordVisible = false;
     passwordInput.type = "password";
+    passwordInput.style.color = "#153416";
     passEye.classList.remove("visible");
+  });
+
+  passEye.addEventListener("click", () => {
+    passwordVisible = !passwordVisible;
+    passwordInput.type = passwordVisible ? "password" : "text";
+    passwordInput.style.color = passwordVisible ? "#153416" : "#fff";
+    passEye.classList.add(passwordVisible ? undefined : "visible");
+    passwordInput.focus();
   });
 });
